3d2f985ccd8d6e2e814d0f53ebf74b8048c734b4
wwww-teddy <yddet.www@gmail.com>
2025-11-12 12:33:26 -0600

    Document parsing and threading for no good reason
    - Spent way too much time on this, but HTML files are now parsed correctly and threaded just for fun
    
    @TODO:
    - Inverted index generator
    
    - TF-IDF indexer

--- src/indexer/indexme.py
+++ src/indexer/indexme.py
@@ -1,26 +1,57 @@
 from src.util.config import CRWL_MAPPING, CRWL_OUTPUT, CRWL_HTML
 from bs4 import BeautifulSoup
 from pathlib import Path
+import threading, os, re
+
+CPU_COUNT = os.cpu_count() or 8
 
 
-def parse_doc(html_path: str):
+def tokenizer(html_path: str):
     p = Path(html_path)
 
-    # Attr:
-    # http://xahlee.info/python/charset_encoding.html
-    # Change file encoding to utf-8
-    with open(p, encoding="utf-8") as html:
+    with open(p, "rb") as html:
         soup_obj = BeautifulSoup(html, "html.parser")
-        content = soup_obj.get_text(" ", strip=True)
+        content = soup_obj.get_text(" ", strip=True).lower()
 
-    # print(content)
+    regex_p = r"[^\w\s]"
+    tokens = re.sub(regex_p, "", content).split()
 
-    return content
+    return tokens
+
+
+def ptokenize(html_list: list[Path], doc_idx: dict[str, list[str]]):
+    for path in html_list:
+        token_list = tokenizer(str(path))
+        doc_idx[path.name] = token_list
+
+
+# Attr:
+# https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
 
 if "__main__" == __name__:
-    path = CRWL_HTML / "9b111d43-6cc5-55ad-a74a-bf9e1a921fd2.html"
-    text_content = parse_doc(str(path))
-    # print(text_content)
+    path = CRWL_HTML
+
+    docs = [file for file in path.iterdir()]
+    chunk_size = len(docs) // CPU_COUNT
+    document_chunks = chunks(docs, chunk_size)
+
+    threads = []
+    doc_idx: dict[str, list[str]] = {}
+    for chunk in document_chunks:
+        t = threading.Thread(target=ptokenize, args=(chunk, doc_idx))
+        threads.append(t)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print(doc_idx)
 
     exit(0)
